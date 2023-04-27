bea18ed1608ac0fe6c6d2d028e140b5e009a9761
Thayana Vilaça Fortunato <76934028+thayana2021js@users.noreply.github.com>
2023-04-27 00:40:38 -0300

    string vazia que vai receber a nova string criada     var newString
    function reverseString(str

---
+++ stringInversa.js
@@ -0,0 +1,14 @@
+function reverseString(str) {
+    // Passo 1. Crie uma string vazia que vai receber a nova string criada
+    var newString = "";
+
+   // Step 2. Crie o laço FOR
+for (var i = str.length - 1; i >= 0; i--) { 
+        newString += str[i]; // ou newString = newString + str[i];
+    }
+
+// Passo 3. Retorne a string invertida
+    return newString; // "olleh"
+}
+ 
+reverseString('hello');
